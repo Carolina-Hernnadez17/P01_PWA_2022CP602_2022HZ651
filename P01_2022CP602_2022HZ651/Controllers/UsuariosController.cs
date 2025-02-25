26c1de986c6a181690fc03a6a66a3a2aa720b38f
Rebeca Margarita Cruz Polanco <rebeca.cruz@catolica.edu.sv>
2025-02-24 18:38:35 -0600

    Subiendo modelo y controlador de usuarios primera fase

--- P01_2022CP602_2022HZ651/Controllers/UsuariosController.cs
+++ P01_2022CP602_2022HZ651/Controllers/UsuariosController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P01_2022CP602_2022HZ651.Models;
 
 namespace P01_2022CP602_2022HZ651.Controllers
 {
@@ -7,5 +9,115 @@ namespace P01_2022CP602_2022HZ651.Controllers
     [ApiController]
     public class UsuariosController : ControllerBase
     {
+        private readonly  ParqueoContext _ParqueoContexto;
+
+        public UsuariosController(ParqueoContext usuarioContexto)
+        {
+            _ParqueoContexto = usuarioContexto;
+        }
+
+        /// <summary>
+        /// EndPoint que retorna el listado de todos los usuarios existentes en la bd
+        /// </summary>
+        /// <returns></returns>
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<Usuarios> listadousuarios = (from e in _ParqueoContexto.usuarios
+                                          select e).ToList();
+            if (listadousuarios.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadousuarios);
+        }
+
+        /// <summary>
+        /// EndPoint guardar usuarios
+        /// </summary>
+        /// <returns></returns>
+
+
+
+        [HttpPost]
+        [Route("Add")]
+
+        public IActionResult GuardarUsuarios([FromBody] Usuarios usuarios)
+        {
+            try
+            {
+                _ParqueoContexto.usuarios.Add(usuarios);
+                _ParqueoContexto.SaveChanges();
+                return Ok(usuarios);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error al guardar el usuario: {ex.Message}");
+            }
+        }
+        /// <summary>
+        /// EndPoint actualizar usuarios
+        /// </summary>
+        /// <returns></returns>
+        /// 
+        [HttpPut]
+        [Route("actualizar/{id}")]
+        public IActionResult ActualizarUsuarios(int id, [FromBody] Usuarios usuariosModificar)
+        {
+            Usuarios? usuariosActual = (from e in _ParqueoContexto.usuarios
+                                    where e.Id_usuario == id
+                                    select e).FirstOrDefault();
+
+            if (usuariosActual == null)
+            {
+                return NotFound();
+            }
+
+            usuariosActual.Nombre = usuariosModificar.Nombre;
+            usuariosActual.Correo = usuariosModificar.Correo;
+            usuariosActual.Telefono = usuariosModificar.Telefono;
+            usuariosActual.Contrasena = usuariosModificar.Contrasena;
+            usuariosActual.Rol = usuariosModificar.Rol;
+
+
+
+
+            _ParqueoContexto.Entry(usuariosActual).State = EntityState.Modified;
+            _ParqueoContexto.SaveChanges();
+
+            return Ok(usuariosModificar);
+        }
+        /// <summary>
+        /// EndPoint eliminar Usuarios
+        /// </summary>
+        /// <returns></returns>
+        /// 
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+        public IActionResult EliminarUsuario(int id)
+        {
+            Usuarios? usuario = (from e in _ParqueoContexto.usuarios
+                              where e.Id_usuario == id
+                              select e).FirstOrDefault();
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            _ParqueoContexto.usuarios.Attach(usuario);
+            _ParqueoContexto.usuarios.Remove(usuario);
+            _ParqueoContexto.SaveChanges();
+
+            return Ok(usuario);
+        }
+        /// <summary>
+        /// EndPoint para retornar el listado de los platos filtrados cuando el precio sea menor de un valor dado.
+        /// </summary>
+        /// <returns></returns>
+
     }
 }
--- P01_2022CP602_2022HZ651/Models/Usuarios.cs
+++ P01_2022CP602_2022HZ651/Models/Usuarios.cs
@@ -1,6 +1,20 @@
-﻿namespace P01_2022CP602_2022HZ651.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P01_2022CP602_2022HZ651.Models
 {
     public class Usuarios
     {
+        [Key]
+        public int Id_usuario { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Correo { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string Contrasena { get; set; }
+
+        public string Rol { get; set; }
     }
 }
